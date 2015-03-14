e51d4935fd6de68405f66d57ebe8f06cd36cbbc5
Kostantinos Margaritis <kotsos.marga@gmail.com>
2015-03-14 18:51:58 +0200

    add guard for ruby install ruby version and select default execute gems installation #fixes #3

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,1 +1,14 @@
-curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3
+# Check for rvm
+if test ! $(which rvm); then
+  echo "Installing RVM for you."
+  ruby -e "$(curl -sSL https://get.rvm.io | bash -s stable)"
+else
+  printf "RVM is already installed."
+fi
+
+rvm install 1.9.3
+rvm install 2.2.0
+rvm use 2.2.0 --default
+
+# Install gems
+source ./deps.sh;
