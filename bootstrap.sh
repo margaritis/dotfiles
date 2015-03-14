3692c6f629d48c74176fc31f1568ca3ded50f172
Konstantinos Margaritis <kotsos.marga@gmail.com>
2015-03-14 19:34:44 +0200

    preparing sonar installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,8 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "java" --exclude "git" \
-    --exclude "maven" --exclude "node"  --exclude "cask" --exclude "ruby" --exclude "docker" --exclude "python" \
+    --exclude "maven" --exclude "node"  --exclude "cask" --exclude "ruby" \
+    --exclude "docker" --exclude "python" --exclude "sonar" \
     --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
---
+++ sonar/install.sh
@@ -0,0 +1,3 @@
+brew update
+brew install sonar
+brew install sonar-runner
