5a079c32dbd5c8fa455da37f6231e951c5e37f13
Konstantinos Margaritis <kotsos.marga@gmail.com>
2015-03-14 19:08:06 +0200

    update bootstrap to exclude topic dirs

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "./java" --exclude "./git" \
-    --exclude "./maven" --exclude "./node"  --exclude "./cask" --exclude "./ruby" \
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "java" --exclude "git" \
+    --exclude "maven" --exclude "node"  --exclude "cask" --exclude "ruby" --exclude "docker" --exclude "python" \
     --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
 	source ~/.bash_profile;
 }
