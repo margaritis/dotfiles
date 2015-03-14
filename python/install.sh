3993d0b292ef5bffda196979076606ba8dddfe6c
Konstantinos Margaritis <kotsos.marga@gmail.com>
2015-03-14 19:08:19 +0200

    python topic

---
+++ python/install.sh
@@ -0,0 +1,8 @@
+brew install python
+brew install python3
+pip install virtualenv
+
+# pip should only run if there is a virtualenv currently activated
+export PIP_REQUIRE_VIRTUALENV=true
+# cache pip-installed packages to avoid re-downloading
+export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
