90bf6fee1327ac6ec66466080c80c161522efb0d
Kostantinos Margaritis <kotsos.marga@gmail.com>
2015-03-16 02:20:53 +0200

    add python install script

--- python/install.sh
+++ python/install.sh
@@ -3,8 +3,12 @@
 brew install python
 brew install python3
 pip install virtualenv
+virtualenv ~/Envs/DevVirtualEnv
+source ~/Envs/DevVirtualEnv/bin/activate
 
 # pip should only run if there is a virtualenv currently activated
 export PIP_REQUIRE_VIRTUALENV=true
 # cache pip-installed packages to avoid re-downloading
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+
+pip install awsebcli
