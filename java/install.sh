d97df0eb3f8c5d15919c191b3b70c5fcb6452e37
Konstantinos Margaritis <kotsos.marga@gmail.com>
2015-05-30 15:58:00 +0300

    add maven, enable jenv scala plugin, remove java from brew as it is only available as cask

--- java/install.sh
+++ java/install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-brew install java
 brew install jenv
+brew install maven
 
 # Configure jenv
 #
 eval "$(jenv init -)"
 jenv enable-plugin export
 jenv enable-plugin ant
-jenv enable-plugin golo
+jenv enable-plugin scala
 jenv enable-plugin gradle
 jenv enable-plugin groovy
 jenv enable-plugin maven
