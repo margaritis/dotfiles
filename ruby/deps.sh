fc754163e80101a457c9c5f51d3683f8c64fbddc
Konstantinos Margaritis <kotsos.marga@gmail.com>
2015-06-14 22:27:08 +0300

    add sass gem and try no-document mode

--- ruby/deps.sh
+++ ruby/deps.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 rvm use 1.9.3
-gem install bundler jekyll rails passenger cheatset
+gem install bundler jekyll rails passenger cheatset sass --no-document
 
 rvm use 2.2.0
-gem install bundler jekyll rails passenger cheatset
+gem install bundler jekyll rails passenger cheatset sass --no-document
 
 rvm use 2.2.0 --default
