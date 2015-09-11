479acd7b4d99897408d030b108778e7c01421e56
Kostantinos Margaritis <kotsos.marga@gmail.com>
2015-09-12 00:04:03 +0300

    update node version

--- node/install.sh
+++ node/install.sh
@@ -9,18 +9,18 @@ mkdir ~/.nvm
 export NVM_DIR=~/.nvm;
 
 # Install node & npm packages for 0.12.2
-nvm install 0.12.2
+nvm install 0.12.6
 source ./node/npm.sh;
 
 nvm install 0.10.26
-nvm reinstall-packages 0.12.2
+nvm reinstall-packages 0.12.6
 
 nvm install 0.10.24
-nvm reinstall-packages 0.12.2
+nvm reinstall-packages 0.12.6
 
 nvm install iojs
-nvm use 0.12.2
-nvm alias default 0.12.2
+nvm use 0.12.6
+nvm alias default 0.12.6
 
 ln -s `which node` /usr/local/bin/node
 ln -s `which jscs` /usr/local/bin/jscs
