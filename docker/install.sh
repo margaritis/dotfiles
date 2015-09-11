fa4d31fdcad3c90e0169127605ef557266d2633c
Konstantinos Margaritis <kotsos.marga@gmail.com>
2015-09-11 23:49:37 +0300

    new docker installation

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-brew install boot2docker
-$(boot2docker shellinit)
-boot2docker init
-boot2docker up
+brew install docker docker-machine docker-swarm docker-compose
+docker-machine create -d virtualbox dev
+eval "$(docker-machine env dev)"
