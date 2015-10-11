8e72f821e44e52d357c68603aafacb6468c88907
Konstantinos Margaritis <kotsos.marga@gmail.com>
2015-10-12 01:28:14 +0300

    add node v4 in formula

--- node/install.sh
+++ node/install.sh
@@ -8,15 +8,19 @@ source $(brew --prefix nvm)/nvm.sh
 mkdir ~/.nvm
 export NVM_DIR=~/.nvm;
 
-# Install node & npm packages for 0.12.2
-nvm install 0.12.6
+# Install node & npm packages for 0.12.6
+
+nvm install 4.0.0
 source ./node/npm.sh;
 
+nvm install 0.12.6
+nvm reinstall-packages 4.0.0
+
 nvm install 0.10.26
-nvm reinstall-packages 0.12.6
+nvm reinstall-packages 4.0.0
 
 nvm install 0.10.24
-nvm reinstall-packages 0.12.6
+nvm reinstall-packages 4.0.0
 
 nvm install iojs
 nvm use 0.12.6
