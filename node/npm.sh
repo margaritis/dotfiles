2d687ba12b71c5478761aae39f2de6730394720b
Konstantinos Margaritis <kotsos.marga@gmail.com>
2015-11-07 16:58:34 +0200

    add frequently used npm packages

--- node/npm.sh
+++ node/npm.sh
@@ -14,7 +14,29 @@ packages=(
   requirejs
   yaml2json
   node-inspector
+  gulp
+  browserify
+  pm2
+  mocha
+  forever
+  forever-service
+  jade
+  karma-cli
+  phonegap
+  babel
 )
 
 echo "installing npm packages..."
 npm install -g  ${packages[@]}
+
+# These packages will be added in quick install script for local repos
+#socket.io
+#memwatch-next
+#commander
+#chalk
+#passport
+#mysql
+#bluebird
+#karma
+#underscore
+#react
