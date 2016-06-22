fd2e62d1570af56ecf18fa0a41449ddb580fabb3
Konstantinos Margaritis <kotsos.marga@gmail.com>
2016-06-22 11:47:39 +0300

    remove jekyll

--- ruby/deps.sh
+++ ruby/deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rvm use 1.9.3
-gem install bundler jekyll rails passenger cheatset sass --no-document
+gem install bundler rails passenger cheatset sass --no-document
 
 rvm use 2.2.0
 gem install bundler jekyll rails passenger cheatset sass --no-document
