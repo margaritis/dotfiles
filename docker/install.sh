6846cc545390629aad0422726d6d6b69ea7608b8
Konstantinos Margaritis <kotsos.marga@gmail.com>
2016-11-07 03:15:09 +0200

    using docker for mac-os instead of custom setup

--- docker/install.sh
+++ docker/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-brew install docker docker-machine docker-swarm docker-compose
-docker-machine create -d virtualbox dev
-eval "$(docker-machine env dev)"
+# brew install docker docker-machine docker-swarm docker-compose
+# docker-machine create -d virtualbox dev
+# eval "$(docker-machine env dev)"
